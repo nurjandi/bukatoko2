feeed4c29850d19ab6c962b95bc0f9f00b03cb97
nurjandi <amirabdillah46@gmail.com>
2018-01-08 12:17:17 +0700

    9

--- UnitTestingTest.php
+++ UnitTestingTest.php
@@ -1,13 +1,13 @@
 <?php
 	require 'UnitTesting.php';
-	include "koneksi.php"
+	include "koneksi.php";
 	class UnitTestingTest extends PHPUnit_Framework_TestCase{
 		protected function setUp(){
 			$this->UnitTesting = new UnitTesting();
 		}
 		public function testSelect(){
 			$result = $this->UnitTesting->select();
-			$this->assertTrue($result);
+			$this->assertEquals('true',$result);
 		}
 		/*public function testDelete(){
 			$result = $this->UnitTesting->delete_data(1);
