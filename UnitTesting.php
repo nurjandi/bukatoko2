241bc2507689a04ab448847926b881fc2151365b
nurjandi <amirabdillah46@gmail.com>
2018-01-08 12:24:12 +0700

    1q

--- UnitTesting.php
+++ UnitTesting.php
@@ -2,7 +2,7 @@
 	class UnitTesting{
 		public function select(){
 			include "koneksi.php";
-			$sql = "select * from 'akun'";
+			$sql = "select * from akun";
 			$query = $db->prepare($sql);
 			$query->execute();
 			if($query->rowCount()>0){
@@ -14,11 +14,11 @@
 				return $hasil;
 			}
 		}
-		/*public function delete_data($id){
+		public function delete_data($id){
 		include ("koneksi.php");
-			$sql = "select rating from user_rating sum(user_rating) where user_guest=."$id;"
+			$sql = "select rating from user_rating sum(user_rating) where user_guest=."$id;
 			$query = $db->prepare($sql);
 			$query->execute();
-		}*/
+		}
 	}
 ?>
